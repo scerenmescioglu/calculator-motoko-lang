064d4c2b6e268c3cd5ec8558454fe84520cd9790
Süheyla Ceren Mescioğlu <135387156+scerenmescioglu@users.noreply.github.com>
2024-12-11 22:46:05 +0200

    Add files via upload

---
+++ calculator.rs
@@ -0,0 +1,42 @@
+// hesap makinesi
+// degiskenler (let -> immutable, var -> mutable)
+// operatorler
+//async metodu
+// if condition
+
+// canister => akilli sozlesme
+
+actor hesap_makinesi {
+  var hucre: Int=0;
+// toplama
+//fonksiyon
+public func toplama(s: Int) : async Int{
+  hucre += s;
+  hucre
+  //(Debug.print(debug_show (hucre));)
+};
+// cikarma 
+public func cikarma(s: Int) : async Int{
+  hucre -= s;
+  hucre
+};
+// carpma 
+public func carpma(s: Int) : async Int{
+  hucre *= s;
+  hucre
+};
+// bolme 
+public func bolme(s: Int) : async ?Int{
+  if(s==0){
+    null
+  }else{
+    hucre /= s;
+    ?hucre
+  };
+};
+
+  // temizle
+  public func temizle() : async (){
+    hucre:=0;
+  };
+};
